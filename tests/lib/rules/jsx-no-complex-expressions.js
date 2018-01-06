58715d03c899d3d43d9d2399f2699ef9cfcd3972
Chris Sauve <chrismsauve@gmail.com>
2018-01-06 18:06:06 -0500

    jsx-no-complex-expressions

---
+++ tests/lib/rules/jsx-no-complex-expressions.js
@@ -0,0 +1,14 @@
+const {RuleTester} = require('eslint');
+const rule = require('../../../lib/rules/jsx-no-complex-expressions');
+
+const ruleTester = new RuleTester();
+const parserOptions = {ecmaVersion: 6, ecmaFeatures: {jsx: true}};
+
+ruleTester.run('jsx-no-complex-expressions', rule, {
+  valid: [
+    {code: '<div title={foo} />', parserOptions},
+    {code: '<div title={condition && foo} />', parserOptions},
+    {code: '<div title={condition && foo || bar} />', parserOptions},
+  ],
+  invalid: [{code: '<div title={condition ? foo : bar} />', parserOptions}],
+});
