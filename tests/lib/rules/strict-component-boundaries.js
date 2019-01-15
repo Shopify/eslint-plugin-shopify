5676cb9a170665651d80f0d8e02fee43cbcf29db
cartogram <matt.seccafien@shopify.com>
2019-01-15 00:12:13 -0500

    Moved to rule-specific fixture

--- tests/lib/rules/strict-component-boundaries.js
+++ tests/lib/rules/strict-component-boundaries.js
@@ -20,45 +20,55 @@ ruleTester.run('strict-component-boundaries', rule, {
     {
       code: `import {someThing} from './components';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/index.js'),
+      filename: fixtureFile('strict-component-boundaries/app/index.js'),
     },
     {
       code: `import {someThing} from '../Bar';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/components/Foo/index.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/components/Foo/index.js',
+      ),
     },
     {
       code: `import {getDisplayName} from '@shopify/react-utilities/components';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/sections/MySection/MySection.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/sections/MySection/MySection.js',
+      ),
     },
     {
       code: `import someUtility from './utilities/someUtility';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/sections/MySection/MySection.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/sections/MySection/MySection.js',
+      ),
     },
     {
       code: `import someThing from './tests/fixtures/SomeMockQuery/query.json';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/components/Foo/index.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/components/Foo/index.js',
+      ),
     },
     {
       code: `import {someThing} from '../../components/Bar';`,
       parserOptions,
-      filename: fixtureFile('basic-app/app/components/Foo/index.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/components/Foo/index.js',
+      ),
     },
     {
       code: `import {someThing} from '../Baz';`,
       parserOptions,
       filename: fixtureFile(
-        'basic-app/app/components/Foo/components/Bar/index.js',
+        'strict-component-boundaries/app/components/Foo/components/Bar/index.js',
       ),
     },
     {
       code: `import {someThing} from '../../Foo.scss';`,
       parserOptions,
       filename: fixtureFile(
-        'basic-app/app/components/Foo/components/Bar/index.js',
+        'strict-component-boundaries/app/components/Foo/components/Bar/index.js',
       ),
     },
   ],
@@ -67,25 +77,29 @@ ruleTester.run('strict-component-boundaries', rule, {
       code: `import someThing from './components/Foo';`,
       parserOptions,
       errors,
-      filename: fixtureFile('basic-app/app/index.js'),
+      filename: fixtureFile('strict-component-boundaries/app/index.js'),
     },
     {
       code: `import someThing from '../Bar/any-path';`,
       parserOptions,
       errors,
-      filename: fixtureFile('basic-app/app/components/Foo/index.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/components/Foo/index.js',
+      ),
     },
     {
       code: `import someThing from './components/Bar/any-path';`,
       parserOptions,
       errors,
-      filename: fixtureFile('basic-app/app/index.js'),
+      filename: fixtureFile('strict-component-boundaries/app/index.js'),
     },
     {
       code: `import someThing from '../Bar/tests/fixtures/SomeMockQuery/query.json';`,
       parserOptions,
       errors,
-      filename: fixtureFile('basic-app/app/components/Foo/index.js'),
+      filename: fixtureFile(
+        'strict-component-boundaries/app/components/Foo/index.js',
+      ),
     },
   ],
 });
