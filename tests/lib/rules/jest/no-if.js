885e951d0c283f7cce1ebc021ba26833c6e5541a
Matt Seccafien <matt@studiocartogram.com>
2019-07-04 01:14:22 -0400

    Functions too

--- tests/lib/rules/jest/no-if.js
+++ tests/lib/rules/jest/no-if.js
@@ -133,6 +133,42 @@ ruleTester.run('no-if', rule, {
         };
       });`,
     },
+    {
+      code: `it('foo', () => {
+        const foo = function(bar) {
+          return foo ? bar : null;
+        };
+      });`,
+    },
+    {
+      code: `it('foo', () => {
+        const foo = function(bar) {
+          if (bar) {
+            return 1;
+          } else {
+            return 2;
+          }
+        };
+      });`,
+    },
+    {
+      code: `it('foo', () => {
+        function foo(bar) {
+          return foo ? bar : null;
+        };
+      });`,
+    },
+    {
+      code: `it('foo', () => {
+        function foo(bar) {
+          if (bar) {
+            return 1;
+          } else {
+            return 2;
+          }
+        };
+      });`,
+    },
   ],
   invalid: [
     {
