e5bbddd42393ab9b2c8823ecf9cd14f0b3a3cb81
Matthew Seccafien <matt.seccafien@shopify.com>
2019-11-14 02:22:06 -0500

    fix changelog

--- tests/lib/rules/jest/no-all-mocks-methods.js
+++ tests/lib/rules/jest/no-all-mocks-methods.js
@@ -1,15 +1,8 @@
 const {RuleTester} = require('eslint');
+
 const rule = require('../../../../lib/rules/jest/no-all-mocks-methods');
 
 const ruleTester = new RuleTester();
-function errorWithMethodName(name) {
-  return [
-    {
-      type: 'Identifier',
-      message: `Do not use ${name} or related methods that are not explicit to your test. Instead, target individual mocks.`,
-    },
-  ];
-}
 
 ruleTester.run('no-all-mocks-methods', rule, {
   valid: [
@@ -23,19 +16,35 @@ ruleTester.run('no-all-mocks-methods', rule, {
   invalid: [
     {
       code: 'jest.resetAllMocks()',
-      errors: errorWithMethodName('resetAllMocks'),
+      errors: [
+        {
+          messageId: 'allMocksMethod',
+        },
+      ],
     },
     {
       code: 'jest.clearAllMocks()',
-      errors: errorWithMethodName('clearAllMocks'),
+      errors: [
+        {
+          messageId: 'allMocksMethod',
+        },
+      ],
     },
     {
       code: 'jest.restoreAllMocks()',
-      errors: errorWithMethodName('restoreAllMocks'),
+      errors: [
+        {
+          messageId: 'allMocksMethod',
+        },
+      ],
     },
     {
       code: 'jest.resetModules()',
-      errors: errorWithMethodName('resetModules'),
+      errors: [
+        {
+          messageId: 'allMocksMethod',
+        },
+      ],
     },
   ],
 });
